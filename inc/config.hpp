977d070169c1e7369486a3398493ecb6833da87c
andy.schwarzl@gmail.com <andy.schwarzl@gmail.com>
2012-03-16 22:29:26 +0100

    dynamically generating config header (DEBUG options and Type settings)

---
+++ inc/config.hpp
@@ -0,0 +1,16 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+ 
+#define MATLAB_DEBUG TRUE
+
+/* #undef GPU_DOUBLE_PREC */
+
+#ifdef GPU_DOUBLE_PREC
+	typedef double DType;
+	typedef double3 DType3;
+#else
+	typedef float DType;
+	typedef float3 DType3;
+#endif
+
+#endif // CONFIG_H

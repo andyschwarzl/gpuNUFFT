97ffd7d8396a250c4394adfdc9944b6d8948f78b
andy.schwarzl@gmail.com <andy.schwarzl@gmail.com>
2012-04-11 00:17:15 +0200

    console style matlab

--- matlab/test_big.m
+++ matlab/test_big.m
@@ -16,20 +16,20 @@ theta = [0:pi/numSpokes:pi-pi/numSpokes];
 for ii = 1:length(theta)
     k(:,ii) = rho*exp(-j*theta(ii));
 end
-figure, plot(k), title(['k-space trajectory: ', num2str(numSpokes), ' projections']);
+%figure, plot(k), title(['k-space trajectory: ', num2str(numSpokes), ' projections']);
 
 % Calculate density compensation function
 w = abs(rho);
 w = repmat(w, 1, numSpokes);
 
 %% Perform Regridding with Kaiser Besser Kernel
-osf = 1; % 1 or 2
-wg = 3;  % 3 to 7
-tic
-[imgRegrid_kb,kernel] = gridkb(kspace,k,w,64,osf,wg,'image');
-toc
+%osf = 1; % 1 or 2
+%wg = 3;  % 3 to 7
+%tic
+%[imgRegrid_kb,kernel] = gridkb(kspace,k,w,64,osf,wg,'image');
+%toc
 %%
-figure, imshow(abs(flipud(imgRegrid_kb)),[]);
+%figure, imshow(abs(flipud(imgRegrid_kb)),[]);
 
 %% kleiner test
 %kspace2 = ([-0.2+0.8i,-0.2+0.8i,0.5,1,0.7]);
@@ -40,7 +40,7 @@ wg = 3;
 %w = ([1,1,1,1,1]);
 
 tic
-[imgRegrid_kb,kernel] = gridkb(kspace2,k,w,64,osf,wg,'image');
+[imgRegrid_kb,kernel] = gridkb(kspace2,k,w,32,osf,wg,'image');
 toc
-figure, imshow(abs(flipud(imgRegrid_kb)),[]);
+%figure, imshow(abs(flipud(imgRegrid_kb)),[]);
           

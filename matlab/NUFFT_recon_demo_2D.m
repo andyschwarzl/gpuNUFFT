c49e7d6d73a2b8bdd6030e6adc8ff046fb6a7cf6
andy.schwarzl@gmail.com <andy.schwarzl@gmail.com>
2012-04-13 15:30:13 +0200

    new testcase 4 channel brain

---
+++ matlab/NUFFT_recon_demo_2D.m
@@ -0,0 +1,42 @@
+%% Script: Demo 2D NUFFT, 4 channel head data
+clear all; close all; clc;
+
+% 4 channel head coil image
+addpath '../bin ../bin/Debug';
+
+% load data
+load img_brain_4ch;
+[nPE,nFE,nCh]=size(img);
+
+% Generate 96 radial projections rawdata
+numSpokes = 96;
+
+% Trajectory
+theta=linspace(0,pi-pi/numSpokes,numSpokes);
+rho=linspace(-0.5,0.5,nPE*2)';
+k=rho*exp(-j*theta);
+
+%% generate Fourier sampling operator
+FT = NUFFT(k, 1, 1, 0, [nPE,nFE], 2);
+
+% generate radial data
+dataRadial = FT*img;
+
+% density compensation
+w = abs(rho);
+w = repmat(w, [1, numSpokes,nCh]);
+dataRadial_dc = dataRadial.*w;
+
+% inverse NUFFT
+recon = FT'*dataRadial;
+recon_dc = FT'*(dataRadial_dc);
+
+% SOS combination
+recon_orig = sqrt(sum(abs(img).^2,3));
+recon_sos = sqrt(sum(abs(recon).^2,3));
+recon_sos_dc = sqrt(sum(abs(recon_dc).^2,3));
+
+% Display results
+figure, imshow(abs(recon_orig),[]); title('Original Fully Sampled');
+figure, imshow(abs(recon_sos),[]); title('NUFFT recon no density compensation');
+figure, imshow(abs(recon_sos_dc),[]); title('NUFFT recon with density compensation');

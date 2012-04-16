bfb16f05b3d167ffe336bfd6ae18cbaa518f40f0
andy.schwarzl@gmail.com <andy.schwarzl@gmail.com>
2012-04-16 22:08:49 +0200

    modified gridding call with griddingoutput option

--- src/gpu/coarse/gridding_gpu.cu
+++ src/gpu/coarse/gridding_gpu.cu
@@ -3,25 +3,6 @@
 
 #include "gridding_gpu.hpp"
 
-//Simple Test Kernel 
-#define N 1000 //DIM ^3 
-#define DIM 10
-__global__ void kernel_call(int *a)
-{
-    int tx = threadIdx.x;
-		int ty = threadIdx.y;
-		int tz = threadIdx.z;
-
-		int index = tx + DIM * (ty + tz * DIM);
-		
-		while (index < N)
-		{
-			a[index] = index;
-			tz += blockDim.z;
-			index = tx + DIM * (ty + tz * DIM);
-		}
-}
-
 #define N_THREADS_PER_SECTOR 2 //16x16
 #define SECTOR_WIDTH 10
 
@@ -34,7 +15,6 @@ __global__ void griddingKernel( DType* data,
 								DType* temp_gdata
 								)
 {
-	//TODO static or dynamic?
 	__shared__ DType sdata[2*SECTOR_WIDTH*SECTOR_WIDTH*SECTOR_WIDTH]; //ca. 8kB -> 2 Blöcke je SM ???
 
 	int sec= blockIdx.x;
@@ -52,15 +32,7 @@ __global__ void griddingKernel( DType* data,
 	if (sec < GI.sector_count)
 	{
 		int ind, k, i, j;
-		__shared__ int max_x;
-		__shared__ int max_y;
-		__shared__ int max_z; 
-		__shared__ int imin;
-		__shared__ int imax;
-		__shared__ int jmin;
-		__shared__ int jmax;
-		__shared__ int kmin;
-		__shared__ int kmax;
+		__shared__ int max_x, max_y, max_z, imin, imax,jmin,jmax,kmin,kmax;
 
 		DType dx_sqr, dy_sqr, dz_sqr, val, ix, jy, kz;
 
@@ -69,78 +41,77 @@ __global__ void griddingKernel( DType* data,
 		center.y = sector_centers[sec * 3 + 1];
 		center.z = sector_centers[sec * 3 + 2];
 
-			//Grid Points over threads
-			int data_cnt;
-			data_cnt = sectors[sec];
+		//Grid Points over threads
+		int data_cnt;
+		data_cnt = sectors[sec];
 			
-			while (data_cnt < sectors[sec+1])
+		while (data_cnt < sectors[sec+1])
+		{
+			__shared__ DType3 data_point; //datapoint shared in every thread
+			data_point.x = crds[3*data_cnt];
+			data_point.y = crds[3*data_cnt +1];
+			data_point.z = crds[3*data_cnt +2];
+
+			max_x = GI.sector_pad_width-1;
+			max_y = GI.sector_pad_width-1;
+			max_z = GI.sector_pad_width-1;
+
+			// set the boundaries of final dataset for gridding this point
+			ix = (data_point.x + 0.5f) * (GI.width) - center.x + GI.sector_offset;
+			set_minmax(ix, &imin, &imax, max_x, GI.kernel_radius);
+			jy = (data_point.y + 0.5f) * (GI.width) - center.y + GI.sector_offset;
+			set_minmax(jy, &jmin, &jmax, max_y, GI.kernel_radius);
+			kz = (data_point.z + 0.5f) * (GI.width) - center.z + GI.sector_offset;
+			set_minmax(kz, &kmin, &kmax, max_z, GI.kernel_radius);
+
+			// grid this point onto the neighboring cartesian points
+			for (k=threadIdx.z;k<=kmax; k += blockDim.z)
 			{
-				__shared__ DType3 data_point; //datapoint shared in every thread
-				data_point.x = crds[3*data_cnt];
-				data_point.y = crds[3*data_cnt +1];
-				data_point.z = crds[3*data_cnt +2];
-
-				max_x = GI.sector_pad_width-1;
-				max_y = GI.sector_pad_width-1;
-				max_z = GI.sector_pad_width-1;
-
-				// set the boundaries of final dataset for gridding this point
-				ix = (data_point.x + 0.5f) * (GI.width) - center.x + GI.sector_offset;
-				set_minmax(ix, &imin, &imax, max_x, GI.kernel_radius);
-				jy = (data_point.y + 0.5f) * (GI.width) - center.y + GI.sector_offset;
-				set_minmax(jy, &jmin, &jmax, max_y, GI.kernel_radius);
-				kz = (data_point.z + 0.5f) * (GI.width) - center.z + GI.sector_offset;
-				set_minmax(kz, &kmin, &kmax, max_z, GI.kernel_radius);
-
-				// grid this point onto the neighboring cartesian points
-				for (k=threadIdx.z;k<=kmax; k += blockDim.z)
+				if (k<=kmax && k>=kmin)
 				{
-					//__syncthreads();
-					if (k<=kmax && k>=kmin)
+					kz = static_cast<DType>((k + center.z - GI.sector_offset)) / static_cast<DType>((GI.width)) - 0.5f;//(k - center_z) *width_inv;
+					dz_sqr = kz - data_point.z;
+					dz_sqr *= dz_sqr;
+					if (dz_sqr < GI.radiusSquared)
 					{
-						kz = static_cast<DType>((k + center.z - GI.sector_offset)) / static_cast<DType>((GI.width)) - 0.5f;//(k - center_z) *width_inv;
-						dz_sqr = kz - data_point.z;
-						dz_sqr *= dz_sqr;
-						if (dz_sqr < GI.radiusSquared)
+						j=threadIdx.y;
+						if (j<=jmax && j>=jmin)
 						{
-							j=threadIdx.y;
-							if (j<=jmax && j>=jmin)
+							jy = static_cast<DType>(j + center.y - GI.sector_offset) / static_cast<DType>((GI.width)) - 0.5f;   //(j - center_y) *width_inv;
+							dy_sqr = jy - data_point.y;
+							dy_sqr *= dy_sqr;
+							if (dy_sqr < GI.radiusSquared)	
 							{
-								jy = static_cast<DType>(j + center.y - GI.sector_offset) / static_cast<DType>((GI.width)) - 0.5f;   //(j - center_y) *width_inv;
-								dy_sqr = jy - data_point.y;
-								dy_sqr *= dy_sqr;
-								if (dy_sqr < GI.radiusSquared)	
+								i=threadIdx.x;
+								
+								if (i<=imax && i>=imin)
 								{
-									i=threadIdx.x;
-								
-									if (i<=imax && i>=imin)
+									ix = static_cast<DType>(i + center.x - GI.sector_offset) / static_cast<DType>((GI.width)) - 0.5f;// (i - center_x) *width_inv;
+									dx_sqr = ix - data_point.x;
+									dx_sqr *= dx_sqr;
+									if (dx_sqr < GI.radiusSquared)	
 									{
-										ix = static_cast<DType>(i + center.x - GI.sector_offset) / static_cast<DType>((GI.width)) - 0.5f;// (i - center_x) *width_inv;
-										dx_sqr = ix - data_point.x;
-										dx_sqr *= dx_sqr;
-										if (dx_sqr < GI.radiusSquared)	
-										{
-											// get kernel value
-											//Berechnung mit Separable Filters 
-											val = kernel[(int) round(dz_sqr * GI.dist_multiplier)] *
-													kernel[(int) round(dy_sqr * GI.dist_multiplier)] *
-													kernel[(int) round(dx_sqr * GI.dist_multiplier)];
-											ind = 2* getIndex(i,j,k,GI.sector_pad_width);
+										//get kernel value
+										//Calculate Separable Filters 
+										val = kernel[(int) round(dz_sqr * GI.dist_multiplier)] *
+												kernel[(int) round(dy_sqr * GI.dist_multiplier)] *
+												kernel[(int) round(dx_sqr * GI.dist_multiplier)];
+										ind = 2* getIndex(i,j,k,GI.sector_pad_width);
 								
-											// multiply data by current kernel val 
-											// grid complex or scalar 
-											sdata[ind]   += val * data[2*data_cnt];
-											sdata[ind+1] += val * data[2*data_cnt+1];
-										} // kernel bounds check x, spherical support 
-									} // x 	 
-								} // kernel bounds check y, spherical support 
-							} // y 
-						} //kernel bounds check z 
-					} // z
-				}//for loop over z entries
-				__syncthreads();
-				data_cnt++;
-			} //grid points per sector
+										// multiply data by current kernel val 
+										// grid complex or scalar 
+										sdata[ind]   += val * data[2*data_cnt];
+										sdata[ind+1] += val * data[2*data_cnt+1];
+									} // kernel bounds check x, spherical support 
+								} // x 	 
+							} // kernel bounds check y, spherical support 
+						} // y 
+					} //kernel bounds check z 
+				} // z
+			}//for loop over z entries
+			__syncthreads();
+			data_cnt++;
+		} //grid points per sector
 	
 	  //write shared data to temporary output grid
 		int sector_ind_offset = sec * GI.sector_pad_width*GI.sector_pad_width*GI.sector_pad_width;
@@ -186,29 +157,6 @@ __global__ void composeOutput(DType* temp_gdata, CufftType* gdata, int* sector_c
 	}
 }
 
-void runSimpleKernelCall()
-{
-	printf("starting gpu implementation\n");
-	int test_a[N];
-	int* test_ad;
-
-	allocateAndCopyToDeviceMem<int>(&test_ad,test_a,N);
-
-	dim3 grid_size = 1;
-	dim3 thread_size(10,10,3);
-	printf("dimensions %d,%d,%d \n",thread_size.x,thread_size.y,thread_size.z);
-	kernel_call<<<grid_size,thread_size>>>(test_ad);
-	
-	copyFromDevice<int>(test_ad,test_a,N);
-
-	printf("output: ");
-	for (int i = 0; i < N; i++)
-		 printf("%d ",test_a[i]);
-	printf("\n");
-	
-	freeDeviceMem(test_ad);
-}
-
 void gridding3D_gpu(DType* data, 
 					int data_cnt,
 					DType* crds, 
@@ -225,13 +173,10 @@ void gridding3D_gpu(DType* data,
 					int width,
 					const GriddingOutput gridding_out)
 {
-	//runSimpleKernelCall();
-		
 	assert(sectors != NULL);
 	
 	//split and run sectors into blocks
 	//and each data point to one thread inside this block 
-
 	GriddingInfo* gi_host = initAndCopyGriddingInfo(sector_count,sector_width,kernel_width,kernel_count,width);
 	
 	DType* data_d, *crds_d, *kernel_d, *temp_gdata_d;

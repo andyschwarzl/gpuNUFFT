42b30505c6463937361d249602e25c84f62b2386
andy.schwarzl@gmail.com <andy.schwarzl@gmail.com>
2012-05-12 18:18:33 +0200

    added context and device functions in order to check their existence, added memory info output to check available memory

--- src/gpu/coarse/gridding_gpu.cu
+++ src/gpu/coarse/gridding_gpu.cu
@@ -23,11 +23,27 @@ void gridding3D_gpu(DType*		data,			//kspace data array
 					const GriddingOutput gridding_out)
 {
 	assert(sectors != NULL);
-	cudaEvent_t start, stop;
+	/*cudaEvent_t start, stop;
 	cudaEventCreate(&start);
 	cudaEventCreate(&stop);
-	cudaEventRecord(start,0);
-    
+	cudaEventRecord(start,0);*/
+	size_t free_mem = 0;
+	size_t total_mem = 0;
+	cuInit(0);
+	CUdevice dev; 
+	CUcontext ctx;
+	cuDeviceGet(&dev,0);
+	CUresult cuRes;
+    if ((cuRes = cuCtxCreate(&ctx,0,dev)) != CUDA_SUCCESS)
+    {
+		printf("get device failed\n");
+		printf("%d\n", cuRes);//cudaGetErrorString(cudaGetLastError()));
+
+	}
+	cudaMemGetInfo(&free_mem, &total_mem);
+	printf("memory usage, free: %lu total: %lu\n",free_mem,total_mem);
+	
+
 	//split and run sectors into blocks
 	//and each data point to one thread inside this block 
 	GriddingInfo* gi_host = initAndCopyGriddingInfo(sector_count,sector_width,kernel_width,kernel_count,grid_width,im_width,osr);
@@ -40,15 +56,17 @@ void gridding3D_gpu(DType*		data,			//kspace data array
 	allocateAndCopyToDeviceMem<CufftType>(&imdata_d,imdata,imdata_count);//Konvention!!!
 
 	printf("allocate and copy gdata of size %d...\n",gi_host->grid_width_dim);
-	allocateAndSetMem<CufftType>(&gdata_d,gi_host->grid_width_dim,0);//Konvention!!!
+	//allocateAndSetMem<CufftType>(&gdata_d,gi_host->grid_width_dim,0);//Konvention!!! set mem in loop
+	allocateDeviceMem<CufftType>(&gdata_d,gi_host->grid_width_dim);
 
 	printf("allocate and copy data of size %d...\n",2*data_count*n_coils);
 	allocateAndCopyToDeviceMem<DType>(&data_d,data,2*data_count*n_coils);
 
 	int temp_grid_count = 2 * sector_count * gi_host->sector_dim;
 	printf("allocate temp grid data of size %d...\n",temp_grid_count);
-	allocateAndSetMem<DType>(&temp_gdata_d,temp_grid_count,0);
-	
+	//allocateAndSetMem<DType>(&temp_gdata_d,temp_grid_count,0);
+	allocateDeviceMem<DType>(&temp_gdata_d,temp_grid_count);
+
 	printf("allocate and copy coords of size %d...\n",3*data_count);
 	allocateAndCopyToDeviceMem<DType>(&crds_d,crds,3*data_count);
 	
@@ -76,7 +94,7 @@ void gridding3D_gpu(DType*		data,			//kspace data array
 		//reset temp array
 		cudaMemset(temp_gdata_d,0, sizeof(DType)*temp_grid_count);
 		cudaMemset(gdata_d,0, sizeof(CufftType)*gi_host->grid_width_dim);
-		cudaMemset(imdata_d,0, sizeof(CufftType)*imdata_count);
+		//cudaMemset(imdata_d,0, sizeof(CufftType)*imdata_count);
 		
 		performConvolution(data_d+data_coil_offset,crds_d,gdata_d,kernel_d,sectors_d,sector_centers_d,temp_gdata_d,gi_host);
 
@@ -90,9 +108,11 @@ void gridding3D_gpu(DType*		data,			//kspace data array
 			copyFromDevice<CufftType>(gdata_d,imdata,gi_host->grid_width_dim);
 			printf("test value at point zero: %f\n",imdata[0].x);
 			freeTotalDeviceMemory(data_d,crds_d,imdata_d,gdata_d,kernel_d,sectors_d,sector_centers_d,temp_gdata_d,NULL);//NULL as stop token
+
 			free(gi_host);
 			/* Destroy the cuFFT plan. */
 			cufftDestroy(fft_plan);
+			cuCtxDestroy(ctx);
 			return;
 		}
 
@@ -113,6 +133,8 @@ void gridding3D_gpu(DType*		data,			//kspace data array
 			free(gi_host);
 			/* Destroy the cuFFT plan. */
 			cufftDestroy(fft_plan);
+			printf("last cuda error: %s\n", cudaGetErrorString(cudaGetLastError()));
+			cuCtxDestroy(ctx);
 			return;
 		}
 
@@ -130,16 +152,17 @@ void gridding3D_gpu(DType*		data,			//kspace data array
 		copyFromDevice<CufftType>(imdata_d,imdata+im_coil_offset,imdata_count);
 	}//iterate over coils
 
-	cudaEventRecord(stop,0);
+/*	cudaEventRecord(stop,0);
 	cudaEventSynchronize(stop);
 	float elapsed;
 	cudaEventElapsedTime(&elapsed,start,stop);
 	printf("Time elapsed: %3.1fms\n",elapsed);
 	cudaEventDestroy(start);
-	cudaEventDestroy(stop);
+	cudaEventDestroy(stop);*/
 
 	/* Destroy the cuFFT plan. */
 	cufftDestroy(fft_plan);
-	freeTotalDeviceMemory(data_d,crds_d,imdata_d,gdata_d,kernel_d,sectors_d,sector_centers_d,temp_gdata_d,NULL);//NULL as stop
+	freeTotalDeviceMemory(data_d,crds_d,gdata_d,imdata_d,kernel_d,sectors_d,sector_centers_d,temp_gdata_d,NULL);//NULL as stop
 	free(gi_host);
+	cuCtxDestroy(ctx);
 }

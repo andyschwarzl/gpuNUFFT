e360728870eea961d09e428895bdf6c22bfa1656
andy.schwarzl@gmail.com <andy.schwarzl@gmail.com>
2012-05-23 21:39:34 +0200

    added forward operation to mtimes matlab object method

--- matlab/GRIDDING3D/@GRIDDING3D/mtimes.m
+++ matlab/GRIDDING3D/@GRIDDING3D/mtimes.m
@@ -1,41 +1,45 @@
 function ress = mtimes(a,bb)
-kspace_data_dim = size(bb,2);
-if (kspace_data_dim > 1)
-    kspace = bb(a.op.data_ind,:);
-    data = [real(kspace(:))'; imag(kspace(:))'];
-    'multiple coil data passed'
-    kspace = reshape(data,[2 a.params.trajectory_length kspace_data_dim]);
-    'call gridding mex kernel'
-    
-    %data_ind = repmat(a.op.data_ind,[1 1 kspace_data_dim]);
-    m = cuda_mex_kernel(single(kspace),single(a.op.coords),int32(a.op.sector_data_cnt),int32(a.op.sector_centers),a.params);
-    size(m)
-    m = squeeze(m(1,:,:,:,:) + 1j*(m(2,:,:,:,:)));
-    
-    %crop data 
-    %ind_off = (a.params.im_width * (double(a.params.osr)-1) / 2) + 1;
-    %ind_start = ind_off;
-    %ind_end = ind_start + a.params.im_width -1;
-    %ress = m(ind_start:ind_end,ind_start:ind_end,ind_start:ind_end,:);
-    ress = m;
+if (a.adjoint)
+    kspace_data_dim = size(bb,2);
+    if (kspace_data_dim > 1)
+        kspace = bb(a.op.data_ind,:);
+        data = [real(kspace(:))'; imag(kspace(:))'];
+        'multiple coil data passed'
+        kspace = reshape(data,[2 a.params.trajectory_length kspace_data_dim]);
+        'call gridding mex kernel'
+
+        %data_ind = repmat(a.op.data_ind,[1 1 kspace_data_dim]);
+        m = cuda_mex_kernel(single(kspace),single(a.op.coords),int32(a.op.sector_data_cnt),int32(a.op.sector_centers),a.params);
+        size(m)
+        m = squeeze(m(1,:,:,:,:) + 1j*(m(2,:,:,:,:)));
+        ress = m;
+    else
+        %prepare data
+        kspace = bb(a.op.data_ind);
+        data = [real(kspace(:))'; imag(kspace(:))'];
+
+        % preweight, DCF
+        %dw = d.*w;
+
+        % performs the normal nufft
+        'call gridding mex kernel'
+        m = cuda_mex_kernel(single(data),single(a.op.coords),int32(a.op.sector_data_cnt),int32(a.op.sector_centers),a.params);
+
+        size(m)
+        m = squeeze(m(1,:,:,:) + 1j*(m(2,:,:,:)));
+        ress = m;
+    end
 else
-    %prepare data
-    kspace = bb(a.op.data_ind);
-    data = [real(kspace(:))'; imag(kspace(:))'];
-    
-    % preweight, DCF
-    %dw = d.*w;
-
-    % performs the normal nufft
+    imspace = bb;
+    imdata = [real(imspace(:))'; imag(imspace(:))'];
+    size(imdata)
+    'multiple coil data passed'
+    %kspace = reshape(imdata,[2 a.params.trajectory_length kspace_data_dim]);
     'call gridding mex kernel'
-    m = cuda_mex_kernel(single(data),single(a.op.coords),int32(a.op.sector_data_cnt),int32(a.op.sector_centers),a.params);
 
-    size(m)
-    m = squeeze(m(1,:,:,:) + 1j*(m(2,:,:,:)));
-
-    %crop data 
-    ind_off = (a.params.im_width * (double(a.params.osr)-1) / 2) + 1;
-    ind_start = ind_off;
-    ind_end = ind_start + a.params.im_width -1;
-    ress = m(ind_start:ind_end,ind_start:ind_end,ind_start:ind_end);
+    %data_ind = repmat(a.op.data_ind,[1 1 kspace_data_dim]);
+    data = mex_gridding3D_forw(single(imdata),single(a.op.coords),int32(a.op.sector_data_cnt),int32(a.op.sector_centers),a.params);
+    size(data)
+    data = squeeze(data(1,:,:,:,:) + 1j*(data(2,:,:,:,:)));
+    ress = data;
 end

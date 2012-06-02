84f78c3edc2a09c80e0bf473ab5cbc547364c433
andy.schwarzl@gmail.com <andy.schwarzl@gmail.com>
2012-06-03 00:33:28 +0200

    modified forward test

--- test/gpu/forward/gridding3DGPU_forward_tests.cpp
+++ test/gpu/forward/gridding3DGPU_forward_tests.cpp
@@ -11,13 +11,17 @@
 
 TEST(TestGPUGriddingForwardConv,KernelCall1Sector)
 {
-	int kernel_width = 3;
-	long kernel_entries = calculateGrid3KernelSize();
+	int kernel_width = 5;
+
+	//oversampling ratio
+	float osr = 1.25f;
+
+	long kernel_entries = calculateGrid3KernelSize(osr,kernel_width/2.0f);
 	DType *kern = (DType*) calloc(kernel_entries,sizeof(DType));
 	loadGrid3Kernel(kern,kernel_entries);
 
 	//Image
-	int im_width = 10;
+	int im_width = 32;
 
 	//Data
 	int data_entries = 1;
@@ -27,12 +31,9 @@ TEST(TestGPUGriddingForwardConv,KernelCall1Sector)
 	//Scaled between -0.5 and 0.5
 	//in triplets (x,y,z)
     DType* coords = (DType*) calloc(3*data_entries,sizeof(DType));//3* x,y,z
-	coords[0] = 0; //should result in 7,7,7 center
-	coords[1] = 0;
+	coords[0] = -0.31719f; //should result in 7,7,7 center
+	coords[1] = -0.38650f;
 	coords[2] = 0;
-
-	//oversampling ratio
-	float osr = DEFAULT_OVERSAMPLING_RATIO;
 
 	DType* im_data;
 	unsigned long dims_g[4];
@@ -45,30 +46,25 @@ TEST(TestGPUGriddingForwardConv,KernelCall1Sector)
 
 	im_data = (DType*) calloc(2*im_size,sizeof(DType));
 	
-	im_data[2*get3DC2lin(5,5,5,im_width)] = 6.8670f;
-
-	im_data[2*get3DC2lin(5,4,5,im_width)] = -6.8158f;
-	im_data[2*get3DC2lin(4,5,5,im_width)] = -6.8158f;
-	im_data[2*get3DC2lin(5,6,5,im_width)] = -6.8158f;
-
-	im_data[2*get3DC2lin(6,6,5,im_width)] = 0.2027f;
-	im_data[2*get3DC2lin(4,4,5,im_width)] = 0.2027f; 
-	im_data[2*get3DC2lin(4,6,5,im_width)] = 0.2027f;
+	for (int x=0;x<im_size;x++)
+		im_data[x] = 1.0f;
 	
 	long grid_width = (unsigned long)(im_width * osr);
 
 	//sectors of data, count and start indices
-	int sector_width = 9;
+	int sector_width = 8;
 	
-	int sector_count = 1;
+	const int sector_count = 125;
 	int* sectors = (int*) calloc(2*sector_count,sizeof(int));
 	sectors[0]=0;
-	sectors[1]=1;
+	sectors[1]=0;
+	sectors[2]=1;
 
-	int* sector_centers = (int*) calloc(3*sector_count,sizeof(int));
+	/*int* sector_centers = (int*) calloc(3*sector_count,sizeof(int));
 	sector_centers[0] = 5;
 	sector_centers[1] = 5;
-	sector_centers[2] = 5;
+	sector_centers[2] = 5;*/
+	int sector_centers[3 * sector_count] = {4,4,4,4,4,12,4,4,20,4,4,28,4,4,36,4,12,4,4,12,12,4,12,20,4,12,28,4,12,36,4,20,4,4,20,12,4,20,20,4,20,28,4,20,36,4,28,4,4,28,12,4,28,20,4,28,28,4,28,36,4,36,4,4,36,12,4,36,20,4,36,28,4,36,36,12,4,4,12,4,12,12,4,20,12,4,28,12,4,36,12,12,4,12,12,12,12,12,20,12,12,28,12,12,36,12,20,4,12,20,12,12,20,20,12,20,28,12,20,36,12,28,4,12,28,12,12,28,20,12,28,28,12,28,36,12,36,4,12,36,12,12,36,20,12,36,28,12,36,36,20,4,4,20,4,12,20,4,20,20,4,28,20,4,36,20,12,4,20,12,12,20,12,20,20,12,28,20,12,36,20,20,4,20,20,12,20,20,20,20,20,28,20,20,36,20,28,4,20,28,12,20,28,20,20,28,28,20,28,36,20,36,4,20,36,12,20,36,20,20,36,28,20,36,36,28,4,4,28,4,12,28,4,20,28,4,28,28,4,36,28,12,4,28,12,12,28,12,20,28,12,28,28,12,36,28,20,4,28,20,12,28,20,20,28,20,28,28,20,36,28,28,4,28,28,12,28,28,20,28,28,28,28,28,36,28,36,4,28,36,12,28,36,20,28,36,28,28,36,36,36,4,4,36,4,12,36,4,20,36,4,28,36,4,36,36,12,4,36,12,12,36,12,20,36,12,28,36,12,36,36,20,4,36,20,12,36,20,20,36,20,28,36,20,36,36,28,4,36,28,12,36,28,20,36,28,28,36,28,36,36,36,4,36,36,12,36,36,20,36,36,28,36,36,36};
 
 	gridding3D_gpu(data,data_entries,1,coords,im_data,im_size,grid_width,kern,kernel_entries, kernel_width,sectors,sector_count,sector_centers,sector_width, im_width,osr,CONVOLUTION);
 	
@@ -84,7 +80,7 @@ TEST(TestGPUGriddingForwardConv,KernelCall1Sector)
 	free(im_data);
 	free(kern);
 	free(sectors);
-	free(sector_centers);
+	//free(sector_centers);
 
 	EXPECT_EQ(1, 1);
 }

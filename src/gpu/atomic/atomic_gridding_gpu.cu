4014b111dc1a892a3981282e072ab8a859612cf2
andy.schwarzl@gmail.com <andy.schwarzl@gmail.com>
2012-06-27 13:14:50 +0200

    slightly modifications to test atomic functions

--- src/gpu/atomic/atomic_gridding_gpu.cu
+++ src/gpu/atomic/atomic_gridding_gpu.cu
@@ -203,6 +203,7 @@ void gridding3D_gpu_adj(DType*		data,			//kspace data array
 		if (err=pt2CufftExec(fft_plan, gdata_d, gdata_d, CUFFT_INVERSE) != CUFFT_SUCCESS)
 		{
 			printf("cufft has failed with err %i \n",err);
+			printf("cuda error: %s\n", cudaGetErrorString(cudaGetLastError()));
 		}
 	
 		if (gridding_out == FFT)

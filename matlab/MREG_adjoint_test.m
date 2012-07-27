a877b1612e1ad420d4db4f077edc42dc77786f81
andy.schwarzl@gmail.com <andy.schwarzl@gmail.com>
2012-07-27 18:12:10 +0200

    adjoint operator test

--- matlab/MREG_adjoint_test.m
+++ matlab/MREG_adjoint_test.m
@@ -4,7 +4,7 @@ clear all; close all; clc;
 load ../../daten/MREG_data_Graz;
 addpath(genpath('GRIDDING3D'));
 addpath(genpath('../bin'));
-
+%%
 % test for adjoint property: looks good
 z_pad = padarray(z,[0 0 10]);
 x1 = randn(size(z_pad));
@@ -17,15 +17,18 @@ imwidth = 64;
 k = E.nufftStruct.om'./(2*pi);
 w = ones(E.trajectory_length,1);
 %%
-G3D = GRIDDING3D(k,w,imwidth,osf,wg,sw);
-
+G3D = GRIDDING3D(k,w,imwidth,osf,wg,sw,'false');
+%%
 x1 = repmat(x1,[1 1 1 32]);
 %%
 y1 = G3D * x1;
+y1_b = 1/sqrt(prod(([64 64 64]))) .* y1;
 %%
 x2 = G3D' * y2;
+x2_b = 1/sqrt(prod(([64 64 64]))) .* x2;
 
-printf('Adjoint test result: %e\n', x1(:)'*x2(:) - y1(:)'*y2(:));
+%%
+printf('Adjoint test result: %e\n', x1(:)'*x2_b(:) - y1_b(:)'*y2(:));
 
 % Apply to data
 %data1 = E * z;

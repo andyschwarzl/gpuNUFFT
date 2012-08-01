f6a8ecd8fed5b2b6183722092c2f762b1226bf72
andy.schwarzl@gmail.com <andy.schwarzl@gmail.com>
2012-08-01 22:50:05 +0200

    added error output

--- src/gpu/atomic/atomic_gridding_kernels.cu
+++ src/gpu/atomic/atomic_gridding_kernels.cu
@@ -437,7 +437,7 @@ __global__ void forwardConvolutionKernel( CufftType* data,
 		int ind, max_x, max_y, max_z, imin, imax, jmin, jmax,kmin,kmax, k, i, j;
 		DType dx_sqr, dy_sqr, dz_sqr, val, ix, jy, kz;
 
-		int3 center;
+		__shared__ int3 center;
 		center.x = sector_centers[sec * 3];
 		center.y = sector_centers[sec * 3 + 1];
 		center.z = sector_centers[sec * 3 + 2];

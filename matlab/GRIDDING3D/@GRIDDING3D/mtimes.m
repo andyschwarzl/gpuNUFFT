266930daf786ae01e22199faa5651a4ff2b23c21
andy.schwarzl@gmail.com <andy.schwarzl@gmail.com>
2012-08-06 07:22:40 +0200

    added scaling

--- matlab/GRIDDING3D/@GRIDDING3D/mtimes.m
+++ matlab/GRIDDING3D/@GRIDDING3D/mtimes.m
@@ -1,15 +1,29 @@
-function ress = mtimes(a,bb)
+function res = mtimes(a,bb)
 if strcmp(a.method,'sparse')
     if (a.adjoint)
         bb = bb(:);
-        ress = Tikreg_gridding(a.op,bb,'machine','gpu_float','adjoint');%, ,'single_coil''verbose',1);
+        res = Tikreg_gridding(a.op,bb,'machine','gpu_float','adjoint');%, ,'single_coil''verbose',1);
+        
+        %TODO check
+        res = res / sqrt(prod(a.imageDim));
+        %END
     else
-        ress = Tikreg_gridding(a.op,bb,'machine','gpu_float','verbose','1');%, 'verbose',1);
+        res = Tikreg_gridding(a.op,bb,'machine','gpu_float','verbose','1');%, 'verbose',1);
+        
+        %TODO check
+        res = res / sqrt(prod(a.imageDim));
+        %END
     end    
 elseif strcmp(a.method,'gridding')
     if (a.adjoint)
-        ress = gridding3D_adj(a.op,bb);
+        res = gridding3D_adj(a.op,bb);
+        %TODO check
+        res = res / sqrt(prod(a.imageDim));
+        %END
     else
-        ress = gridding3D_forw(a.op,bb);
+        res = gridding3D_forw(a.op,bb);
+        %TODO check
+        res = res / sqrt(prod(a.imageDim));
+        %END
     end
 end

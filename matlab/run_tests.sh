d915ff16b8761c6ed806ee29b3f826138b9d5fd7
andy.schwarzl@gmail.com <andy.schwarzl@gmail.com>
2012-09-08 01:49:23 +0200

    changed to run temporarily only one test cassse

--- matlab/run_tests.sh
+++ matlab/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #testscript 
 echo Start Testscript
-for i in 1 2 # 3 4 5 6 7 8 9 10
+for i in 1 # 2 # 3 4 5 6 7 8 9 10
 do
 	echo " running $i..." 
 	matlab -nojvm -nosplash < test_gridding3D_Operator_2D_data_script.m > "script.out.$i" #& 

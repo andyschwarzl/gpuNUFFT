9bc3a92a22a659536e03ada96f3cc82478b9b9ad
andy.schwarzl@gmail.com <andy.schwarzl@gmail.com>
2012-09-08 15:52:02 +0200

    changed data name

--- matlab/show_cuda_pc_results.m
+++ matlab/show_cuda_pc_results.m
@@ -1,10 +1,5 @@
 %%
-load ../../daten/results/2D_256_2_5_32
+load ../../daten/results/2D_256_1_25_3_32
+%%
 
-%%
-figure, imshow(imresize(((abs(recon_sos_res(:,:)))),2),[]), title('gridding dc sos');
-%%
-load ../../daten/results/2D_256_1_5_3_32
-
-%%
 figure, imshow(imresize(((abs(recon_sos_res(:,:)))),2),[]), title('gridding dc sos');

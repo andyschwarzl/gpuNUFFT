0478171d932404804e2939135137864387a6fdc2
Andi <andy.schwarzl@gmail.com>
2013-10-01 23:07:33 +0200

    added fields and implementation to perform adjoint gridding with operator

--- inc/gridding_operator.hpp
+++ inc/gridding_operator.hpp
@@ -2,6 +2,7 @@
 #define GRIDDING_OPERATOR_H_INCLUDED
 
 #include "config.hpp"
+#include "gridding_gpu.hpp"
 
 namespace GriddingND
 {
@@ -15,34 +16,61 @@ namespace GriddingND
 	class GriddingOperator 
 	{
 	public:
-
-		GriddingOperator()
+		GriddingOperator():
+			chnCount(1), frameCount(1)
 		{
-			this->chnCount = 1;
-			this->frameCount = 1;
+		};
+
+
+		GriddingOperator(size_t kernelWidth, size_t sectorWidth, DType osf): 
+		chnCount(1), frameCount(1), osf(osf), kernelWidth(kernelWidth), sectorWidth(sectorWidth)
+		{
+			kernelCount = calculateGrid3KernelSize(osf, kernelWidth/2.0f);
+			kernel = (DType*) calloc(kernelCount,sizeof(float));
+			loadGrid3Kernel(kernel,(int)kernelCount,(int)kernelWidth,osf);
+	
 		};
 
 		~GriddingOperator()
 		{
 		};
 
-		void performGriddingAdj();
+		// SETTER 
+		void setDataCount(size_t dataCount)		{this->dataCount = dataCount;};
+		void setChnCount(size_t chnCount)		{this->chnCount = chnCount;};
+		void setFrameCount(size_t frameCount)	{this->frameCount = frameCount;};
+		void setSectorCount(size_t sectorCount)	{this->sectorCount = sectorCount;};
+		void setOsf(DType osf)					{this->osf = osf;};
 
-		void setDataCount(size_t dataCount){this->dataCount = dataCount;};
-		void setChnCount(size_t chnCount){this->chnCount = chnCount;};
-		void setFrameCount(size_t frameCount){this->frameCount = frameCount;};
+		void setKspaceCoords(DType *kspaceCoords)	{this->kspaceCoords = kspaceCoords;};
+		void setData(DType2 *data)					{this->data = data;};
+		void setSens(DType2 *sens)					{this->sens = sens;};
+		void setDens(DType *dens)					{this->dens = dens;};
+		void setSectorCenters(size_t *sectorCenters)					{this->sectorCenters = sectorCenters;};
+		void setSectors(size_t *sectors)					{this->sectors = sectors;};
 
-		void setKspaceCoords(DType *kspaceCoords){this->kspaceCoords = kspaceCoords;};
-		void setData(DType2 *data){this->data = data;};
-		void setSens(DType2 *sens){this->sens = sens;};
-		void setDens(DType *dens){this->dens = dens;};
+		void setKSpaceWidth(size_t width)	{this->kspaceDim.width = width;};
+		void setKSpaceHeight(size_t height)	{this->kspaceDim.height = height;};
+		void setKSpaceDepth(size_t depth)	{this->kspaceDim.depth = depth;};
 
-		void setKSpaceWidth(size_t width){this->kspaceDim.width = width;};
-		void setKSpaceHeight(size_t height){this->kspaceDim.height = height;};
-		void setKSpaceDepth(size_t depth){this->kspaceDim.depth = depth;};
+		// GETTER
+		DType*	getKspaceCoords()	{return this->kspaceCoords;};
+		DType2* getData()			{return this->data;};
+		DType2* getSens()			{return this->sens;};
+		DType*	getDens()			{return this->dens;};
+
+		// OPERATIONS
+
+		//adjoint gridding
+		void performGriddingAdj(CufftType** imgData);
+		void performGriddingAdj(CufftType** imgData, GriddingOutput griddingOut);
 
 	private:
 		
+		size_t getImgCount() {return kspaceDim.width * kspaceDim.height * kspaceDim.depth;};
+		size_t getGridWidth() {return (size_t)(kspaceDim.width * osf);};
+		bool applyDensComp(){return this->dens != NULL;};
+
 		// x y [z], image dimension
 		KSpaceDim kspaceDim;
 
@@ -54,6 +82,15 @@ namespace GriddingND
 
 		// number of frames
 		size_t frameCount;
+
+		// number of sectors
+		size_t sectorCount;
+
+		size_t kernelCount;
+
+		DType *kernel;
+
+
 
 		// simple array
 		// dimensions: n dimensions * dataCount
@@ -67,9 +104,22 @@ namespace GriddingND
 		// dimensions: kspaceDim * chnCount
 		DType2 *sens;
 
-		// simple
+		// density compensation
+		// dimensions: dataCount
 		DType *dens;
+
+		// sector centers
+		size_t* sectorCenters;
+
+		// sectors 
+		// assignment of data index to according sector
+		size_t* sectors;
+
+		// oversampling factor
+		DType osf;
 		
+		size_t kernelWidth;
+		size_t sectorWidth;
 	};
 }
 
--- src/gridding_operator.cpp
+++ src/gridding_operator.cpp
@@ -3,9 +3,18 @@
 
 #include <iostream>
 
-void GriddingND::GriddingOperator::performGriddingAdj()
+void GriddingND::GriddingOperator::performGriddingAdj(CufftType** imgData)
+{
+	performGriddingAdj(imgData,DEAPODIZATION);
+}
+
+void GriddingND::GriddingOperator::performGriddingAdj(CufftType** imgData, GriddingOutput griddingOut)
 {
 	std::cout << "performing gridding adjoint!!!" << std::endl;
 
 	std::cout << "test " << this->kspaceDim.width << std::endl;
+
+	std::cout << "dataCount: " << this->dataCount << " chnCount: " << this->chnCount << std::endl;
+	std::cout << "imgCount: " << this->getImgCount() << " gridWidth: " << this->getGridWidth() << std::endl;
+	gridding3D_gpu_adj(this->data,this->dataCount,this->chnCount,this->kspaceCoords,imgData,this->getImgCount(),this->getGridWidth(),this->kernel,this->kernelCount,this->kernelWidth,(int*)this->sectors,this->sectorCount,(int*)this->sectorCenters,this->sectorWidth, this->kspaceDim.width,this->osf,this->applyDensComp(),this->dens,griddingOut);
 }

5a5527280b2add0dd80faeab6bef5e9aaa9393d3
andy.schwarzl@gmail.com <andy.schwarzl@gmail.com>
2013-10-10 11:08:51 +0200

    minor code refactoring

--- inc/gridding_operator.hpp
+++ inc/gridding_operator.hpp
@@ -26,7 +26,7 @@ namespace GriddingND
 		GriddingOperator():
 			chnCount(1), frameCount(1)
 		{
-		};
+        }
 
 		GriddingOperator(size_t kernelWidth, size_t sectorWidth, DType osf): 
 		chnCount(1), frameCount(1), osf(osf), kernelWidth(kernelWidth), sectorWidth(sectorWidth)
@@ -35,38 +35,35 @@ namespace GriddingND
 			kernel = (DType*) calloc(kernelCount,sizeof(float));
 			loadGrid3Kernel(kernel,(int)kernelCount,(int)kernelWidth,osf);
 	
-		};
+        }
 
 		~GriddingOperator()
 		{
-		};
+        }
 
 		// SETTER 
-		void setDataCount(size_t dataCount)		{this->dataCount = dataCount;};
-		void setChnCount(size_t chnCount)		{this->chnCount = chnCount;};
-		void setFrameCount(size_t frameCount)	{this->frameCount = frameCount;};
-		void setSectorCount(size_t sectorCount)	{this->sectorCount = sectorCount;};
-		void setOsf(DType osf)					{this->osf = osf;};
+                void setDataCount(size_t dataCount)	{this->dataCount = dataCount;}
+                void setChnCount(size_t chnCount)	{this->chnCount = chnCount;}
+                void setFrameCount(size_t frameCount)	{this->frameCount = frameCount;}
+                void setSectorCount(size_t sectorCount)	{this->sectorCount = sectorCount;}
+                void setOsf(DType osf)			{this->osf = osf;}
 
-		void setKspaceCoords(DType *kspaceCoords)	{this->kspaceCoords = kspaceCoords;};
-		void setData(DType2 *data)					{this->data = data;};
-		void setSens(DType2 *sens)					{this->sens = sens;};
-		void setDens(DType *dens)					{this->dens = dens;};
-		void setSectorCenters(size_t *sectorCenters)					{this->sectorCenters = sectorCenters;};
-		void setSectors(size_t *sectors)					{this->sectors = sectors;};
+                void setKspaceCoords(DType *kspaceCoords)	{this->kspaceCoords = kspaceCoords;}
+                void setData(DType2 *data)		{this->data = data;}
+                void setSens(DType2 *sens)		{this->sens = sens;}
+                void setDens(DType *dens)		{this->dens = dens;}
+                void setSectorCenters(size_t *sectorCenters)	{this->sectorCenters = sectorCenters;}
+                void setSectors(size_t *sectors)		{this->sectors = sectors;}
 
-		void setKSpaceWidth(size_t width)	{this->kspaceDim.width = width;};
-		void setKSpaceHeight(size_t height)	{this->kspaceDim.height = height;};
-		void setKSpaceDepth(size_t depth)	{this->kspaceDim.depth = depth;};
-		
-		void setKernelWidth(size_t kernelWidth)	{this->kernelWidth= kernelWidth;};
-		void setSectorWidth(size_t sectorWidth)	{this->sectorWidth= sectorWidth;};
+                void setKSpaceWidth(size_t width)	{this->kspaceDim.width = width;}
+                void setKSpaceHeight(size_t height)	{this->kspaceDim.height = height;}
+                void setKSpaceDepth(size_t depth)	{this->kspaceDim.depth = depth;}
 
 		// GETTER
-		DType*	getKspaceCoords()	{return this->kspaceCoords;};
-		DType2* getData()			{return this->data;};
-		DType2* getSens()			{return this->sens;};
-		DType*	getDens()			{return this->dens;};
+                DType*	getKspaceCoords()	{return this->kspaceCoords;}
+                DType2* getData()			{return this->data;}
+                DType2* getSens()			{return this->sens;}
+                DType*	getDens()			{return this->dens;}
 
 		size_t getKernelWidth()		{return this->kernelWidth;};
 		size_T getSectorWidth()		{return this->sectorWidth;};
@@ -83,9 +80,9 @@ namespace GriddingND
 
 	private:
 		
-		size_t getImgCount() {return kspaceDim.width * kspaceDim.height * kspaceDim.depth;};
-		size_t getGridWidth() {return (size_t)(kspaceDim.width * osf);};
-		bool applyDensComp(){return this->dens != NULL;};
+                size_t getImgCount() {return kspaceDim.width * kspaceDim.height * kspaceDim.depth;}
+                size_t getGridWidth() {return (size_t)(kspaceDim.width * osf);}
+                bool applyDensComp(){return this->dens != NULL;}
 
 		// x y [z], image dimension
 		KSpaceDim kspaceDim;

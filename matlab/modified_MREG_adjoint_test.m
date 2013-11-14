d8bb733cd398215b2866b8e5515816fc32ec2a5e
Andi <andy.schwarzl@gmail.com>
2013-11-14 21:20:34 +0100

    adapted adjoint test

--- matlab/modified_MREG_adjoint_test.m
+++ matlab/modified_MREG_adjoint_test.m
@@ -1,13 +1,14 @@
 % MREG Test
 
 clear all; close all; clc;
-addpath(genpath('GRIDDING3D'));
+addpath(genpath('./GRIDDING3D'));
 addpath(genpath('../bin'));
 %% test for adjoint prop
 imwidth = 64;
-z_pad = [imwidth imwidth imwidth 2];
+nchn = 2;
+z_pad = [imwidth imwidth imwidth nchn];
 x1 = randn((z_pad));
-y2 = randn([10000 2]);
+y2 = randn([10000 nchn]);
 
 osf = 1.5;%1,1.25,1.5,1.75,2
 wg = 3;%3-7
@@ -23,10 +24,11 @@ k_traj = [real(k(:))'; imag(k(:))';zeros(1,length(k(:)))];
 
 w = ones(1,length(k(:)));
 %%
-G3D = GRIDDING3D(k_traj,w,imwidth,osf,wg,sw,'false');
+G3D = GRIDDING3D(k_traj,w,imwidth,osf,wg,sw,[imwidth imwidth imwidth],'false');
 
 %%
 y1 = G3D * x1;
+%%
 x2 = G3D' * y2;
 %%
 x1(:)'*x2(:) - y1(:)'*y2(:)

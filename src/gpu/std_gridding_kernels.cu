d6350077357e429d1d214277a10787c343735cbe
Andi <andy.schwarzl@gmail.com>
2014-03-28 10:35:46 +0100

    Adapted crop function to variable dimensions

--- src/gpu/std_gridding_kernels.cu
+++ src/gpu/std_gridding_kernels.cu
@@ -157,27 +157,27 @@ __global__ void precomputeDeapodizationKernel2D(DType* deapo_d, DType beta, DTyp
 }
 
 
-__global__ void cropKernel(CufftType* gdata,CufftType* imdata, int offset, int N)
+__global__ void cropKernel(CufftType* gdata,CufftType* imdata, IndType3 offset, int N)
 {
 	int t = threadIdx.x +  blockIdx.x *blockDim.x;
 	int x, y, z, grid_ind;
 	while (t < N) 
 	{
-		getCoordsFromIndex(t, &x, &y, &z, GI.imgDims.x);
-		grid_ind = getIndex(offset+x,offset+y,offset+z,GI.gridDims.x);
+    getCoordsFromIndex(t, &x, &y, &z, GI.imgDims.x,GI.imgDims.y,GI.imgDims.z);
+		grid_ind = computeXYZ2Lin(offset.x+x,offset.y+y,offset.z+z,GI.gridDims);
 		imdata[t] = gdata[grid_ind];
 		t = t + blockDim.x*gridDim.x;
 	}
 }
 
-__global__ void cropKernel2D(CufftType* gdata,CufftType* imdata, int offset, int N)
+__global__ void cropKernel2D(CufftType* gdata,CufftType* imdata, IndType3 offset, int N)
 {
 	int t = threadIdx.x +  blockIdx.x *blockDim.x;
 	int x, y, grid_ind;
 	while (t < N) 
 	{
-		getCoordsFromIndex2D(t, &x, &y, GI.imgDims.x);
-		grid_ind = getIndex2D(offset+x,offset+y,GI.gridDims.x);
+    getCoordsFromIndex2D(t, &x, &y, GI.imgDims.x,GI.imgDims.y);
+		grid_ind = computeXY2Lin(offset.x+x,offset.y+y,GI.gridDims);
 		imdata[t] = gdata[grid_ind];
 		t = t + blockDim.x*gridDim.x;
 	}
@@ -305,7 +305,10 @@ void performCrop(CufftType* gdata_d,
 				 CufftType* imdata_d,
 				 GriddingND::GriddingInfo* gi_host)
 {
-	int ind_off = (int)(gi_host->imgDims.x * ((DType)gi_host->osr - 1.0f)/(DType)2);
+	IndType3 ind_off;
+  ind_off.x = (IndType)(gi_host->imgDims.x * ((DType)gi_host->osr - 1.0f)/(DType)2);
+  ind_off.y = (IndType)(gi_host->imgDims.y * ((DType)gi_host->osr - 1.0f)/(DType)2);
+  ind_off.z = (IndType)(gi_host->imgDims.z * ((DType)gi_host->osr - 1.0f)/(DType)2);
 	if (DEBUG)
 		printf("start cropping image with offset %d\n",ind_off);
 

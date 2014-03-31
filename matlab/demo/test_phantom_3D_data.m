1c886a2f6cc7a1874fab7a1bda70d6ff1aa07de9
Andi <andy.schwarzl@gmail.com>
2014-03-31 23:04:23 +0200

    Changed test to compute cpu and gpu version.

--- matlab/demo/test_phantom_3D_data.m
+++ matlab/demo/test_phantom_3D_data.m
@@ -13,26 +13,37 @@ addpath(genpath('../utils'));
 load sl3d64
 N = imgDim(1);
 N3D = imgDim(3);
-useGPU = false;
+useGPU = true;
 %% generate Fourier sampling operator
-osf = 1.25;
+osf = 2.0;
 wg = 3;
 sw = 8;
 imwidth = N;
+%% debug
+%imwidth = N;
+%imgDim = [64,64,56];
+%N3D = imgDim(3);
 %%
 tic
-if useGPU
-    FT = GRIDDING3D(k_traj',dens',imwidth,osf,wg,sw,imgDim,'false');
-else
-    FT = NUFFT3D(k_traj, dens, 1, 0, imgDim, 2,1);
-end
+FT = GRIDDING3D(k_traj',dens',imwidth,osf,wg,sw,imgDim,'false');
+toc
+tic
+FTCPU = NUFFT3D(k_traj, dens, 1, 0, imgDim, 2,1);
 toc
 
 %% recon
 tic
 imgRecon = FT'*dataRadial(:);
 toc
+tic
+imgReconCPU = FTCPU'*dataRadial(:);
+toc
 %% show results
-figure, imshow((abs(imgRecon(:,:,N/2))),[]), title('Recon');
+figure,h1 = subplot(121);
+imshow((abs(imgRecon(:,:,N/2))),[]), title('Recon');
+h2=subplot(122);
+imshow((abs(imgReconCPU(:,:,N/2))),[]), title('Recon CPU');
+linkaxes([h1 h2]);
 %%
-show3DImage([4,8],abs(imgRecon(:,:,N3D/2-15:N3D/2+16)),'test','slice');
+show3DImage([4,8],abs(imgRecon(:,:,N3D/2-15:N3D/2+16)),'GPU','slice');
+show3DImage([4,8],abs(imgReconCPU(:,:,N3D/2-15:N3D/2+16)),'CPU','slice');

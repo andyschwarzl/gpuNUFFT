andyschwarzl/gpuNUFFT

commit 4fd6a0569f2c5e32d1f3bdb4b9a767342e709576

Use double precision in kernel (pre-)computation.

inc/griddingFunctions.hpp
@@ -45,25 +45,25 @@
 *	         This lookup table is with respect to the radius squared.
 *	         and is based on the work described in Beatty et al. MRM 24, 2005
 */
-static DType i0( DType x )
+static double i0( double x )
 {
-  DType ax = fabs(x);
-  DType ans;
-  DType y;
+  double ax = abs(x);
+  double ans;
+  double y;
 
-  if (ax < (DType)3.75f) 
+  if (ax < 3.75) 
   {
-    y=x/3.75f,y=y*y;
-    ans=1.0f+y*(3.5156229f+y*(3.0899424f+y*(1.2067492f
-      +y*(0.2659732f+y*(0.360768e-1f+y*0.45813e-2f)))));
+    y=x/3.75,y=y*y;
+    ans=1.0+y*(3.5156229+y*(3.0899424+y*(1.2067492
+      +y*(0.2659732+y*(0.360768e-1+y*0.45813e-2)))));
   } 
   else 
   {
-    y=3.75f/ax;
-    ans=(exp(ax)/sqrt(ax))*(0.39894228f+y*(0.1328592e-1f
-      +y*(0.225319e-2f+y*(-0.157565e-2f+y*(0.916281e-2f
-      +y*(-0.2057706e-1f+y*(0.2635537e-1f+y*(-0.1647633e-1f
-      +y*0.392377e-2f))))))));
+    y=3.75/ax;
+    ans=(exp(ax)/sqrt(ax))*(0.39894228+y*(0.1328592e-1
+      +y*(0.225319e-2+y*(-0.157565e-2+y*(0.916281e-2
+      +y*(-0.2057706e-1+y*(0.2635537e-1+y*(-0.1647633e-1
+      +y*0.392377e-2))))))));
   }
   return (ans);
 }

commit fc46ccff014c23b84e22bd04ba733fd6a02427a3

Fix bug introduced by 4fd6a0569f2c5e32d1f3bdb4b9a767342e709576.

inc/griddingFunctions.hpp
@@ -46,29 +46,28 @@
 *	         This lookup table is with respect to the radius squared.
 *	         and is based on the work described in Beatty et al. MRM 24, 2005
 */
-static double i0( double x )
+static DType i0( DType x )
 {
-  double ax = abs(x);
-  double ans;
-  double y;
+  DType ax = fabs(x);
+  DType ans;
+  DType y;
 
-  if (ax < 3.75) 
+  if (ax < (DType)3.75f) 
   {
-    y=x/3.75,y=y*y;
-    ans=1.0+y*(3.5156229+y*(3.0899424+y*(1.2067492
-      +y*(0.2659732+y*(0.360768e-1+y*0.45813e-2)))));
+    y=x/3.75f,y=y*y;
+    ans=1.0f+y*(3.5156229f+y*(3.0899424f+y*(1.2067492f
+      +y*(0.2659732f+y*(0.360768e-1f+y*0.45813e-2f)))));
   } 
   else 
   {
-    y=3.75/ax;
-    ans=(exp(ax)/sqrt(ax))*(0.39894228+y*(0.1328592e-1
-      +y*(0.225319e-2+y*(-0.157565e-2+y*(0.916281e-2
-      +y*(-0.2057706e-1+y*(0.2635537e-1+y*(-0.1647633e-1
-      +y*0.392377e-2))))))));
+    y=3.75f/ax;
+    ans=(exp(ax)/sqrt(ax))*(0.39894228f+y*(0.1328592e-1f
+      +y*(0.225319e-2f+y*(-0.157565e-2f+y*(0.916281e-2f
+      +y*(-0.2057706e-1f+y*(0.2635537e-1f+y*(-0.1647633e-1f
+      +y*0.392377e-2f))))))));
   }
   return (ans);
 }
-
 
 /* LOADGRID3KERNEL()
 * Loads a radius of the circularly symmetric kernel into a 1-D array, with
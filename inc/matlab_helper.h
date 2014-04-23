9747044e0e9ad8a288de8fd946c3e6a92ae7a3e6
Andi <andy.schwarzl@gmail.com>
2014-04-23 16:08:46 +0200

    Fix usage of Interpolation type enum on linux systems.

--- inc/matlab_helper.h
+++ inc/matlab_helper.h
@@ -6,6 +6,7 @@
 
 #include "config.hpp" 
 #include "gridding_operator.hpp"
+#include "gridding_types.hpp"
 
 #define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
 
@@ -128,10 +129,10 @@ inline GriddingND::InterpolationType getInterpolationTypeOf(int val)
 {
   switch (val)
   {
-  case 1 : return GriddingND::InterpolationType::TEXTURE_LOOKUP;
-  case 2: return GriddingND::InterpolationType::TEXTURE2D_LOOKUP;
-  case 3: return GriddingND::InterpolationType::TEXTURE3D_LOOKUP;
-  default : return GriddingND::InterpolationType::CONST_LOOKUP;
+  case 1 : return GriddingND::TEXTURE_LOOKUP;
+  case 2: return GriddingND::TEXTURE2D_LOOKUP;
+  case 3: return GriddingND::TEXTURE3D_LOOKUP;
+  default : return GriddingND::CONST_LOOKUP;
   }
 }
 

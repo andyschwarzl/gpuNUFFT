bc5334f55ba9602086256f2fa363bd7f0e989030
Andi <andy.schwarzl@gmail.com>
2014-05-12 21:29:44 +0200

    Removed duplicate code.

--- matlab/GRIDDING3D/gridding/gridding3D_adj.m
+++ matlab/GRIDDING3D/gridding/gridding3D_adj.m
@@ -1,63 +1,41 @@
 function ress = gridding3D_adj(a,bb)
-% ress = gridding3D_forw(a,bb)
-% Performs forward gridding 
+% ress = gridding3D_adj(a,bb)
+% Performs adjoint gridding 
 % from k-space to image space 
 %
 % supports multi-channel data
 %
 % a  ... Gridding Operator
 % bb ... k-space data
+%        k x nChn
 %
-kspace_data_dim = size(bb,2);
-
-if (kspace_data_dim > 1)
-    kspace = bb(:,:);
-    kspace = [real(kspace(:))'; imag(kspace(:))'];
-
+nChn = size(bb,2);
+%prepare data
+if (nChn > 1)
     if a.verbose
         disp('multiple coil data passed');
     end
-    kspace = reshape(kspace,[2 a.params.trajectory_length kspace_data_dim]);
-    
-    if a.verbose
-        disp('call gridding mex kernel');
-    end
-    if a.atomic == true
-        if a.verbose
-            disp('using atomic operations');
-        end
-         m = mex_gridding3D_adj_atomic_f(single(kspace),(a.dataIndices),single(a.coords),(a.sectorDataCount),(a.sectorProcessingOrder),(a.sectorCenters(:)),single(a.densSorted),single(a.sens),a.params);
-        %m = mex_gridding3D_adj_atomic_f(single(kspace),uint32(a.dataIndices),single(a.coords),uint32(a.sectorDataCount),uint32(a.sectorProcessingOrder),uint32(a.sectorCenters(:)),single(a.densSorted),a.params);
-    else
-        m = mex_gridding3D_adj_f(single(kspace),(a.dataIndices),single(a.coords),(a.sectorDataCount),(a.sectorProcessingOrder),(a.sectorCenters(:)),single(a.densSorted),single(a.sens),a.params);
-        %m = mex_gridding3D_adj_f(single(kspace),uint32(a.dataIndices),single(a.coords),uint32(a.sectorDataCount),uint32(a.sectorProcessingOrder),uint32(a.sectorCenters(:)),single(a.densSorted),a.params);
-    end;
-    %size(m);
-    if (a.params.is2d_processing)
-        ress = squeeze(m(1,:,:,:) + 1i*(m(2,:,:,:)));
-    else
-        ress = squeeze(m(1,:,:,:,:) + 1i*(m(2,:,:,:,:)));
-    end
-else
-    %prepare data
+    kspace = bb(:,:);
+    kspace = [real(kspace(:))'; imag(kspace(:))'];
+    kspace = reshape(kspace,[2 a.params.trajectory_length nChn]);
+else    
     kspace = bb;
     kspace = [real(kspace(:))'; imag(kspace(:))'];
-
-    % performs the normal nufft
-    if a.verbose
-        disp('call gridding mex kernel')
-    end
-    if a.atomic == true
-        m = mex_gridding3D_adj_atomic_f(single(kspace),(a.dataIndices),single(a.coords),(a.sectorDataCount),(a.sectorProcessingOrder),(a.sectorCenters(:)),single(a.densSorted),single(a.sens),a.params);
-        %m = mex_gridding3D_adj_atomic_f(single(kspace),uint32(a.dataIndices),single(a.coords),uint32(a.sectorDataCount),uint32(a.sectorProcessingOrder),uint32(a.sectorCenters(:)),single(a.densSorted),a.params);
-    else
-        m = mex_gridding3D_adj_f(single(kspace),(a.dataIndices),single(a.coords),(a.sectorDataCount),(a.sectorProcessingOrder),(a.sectorCenters(:)),single(a.densSorted),single(a.sens),a.params);
-        %m = mex_gridding3D_adj_f(single(kspace),uint32(a.dataIndices),single(a.coords),uint32(a.sectorDataCount),uint32(a.sectorProcessingOrder),uint32(a.sectorCenters(:)),single(a.densSorted),a.params);
-    end
-    %size(m);
-    if (a.params.is2d_processing)
-        ress = squeeze(m(1,:,:,:) + 1i*(m(2,:,:,:)));
-    else
-        ress = squeeze(m(1,:,:,:,:) + 1i*(m(2,:,:,:,:)));
-    end
+end    
+if a.verbose
+    disp('call gridding mex kernel');
 end
+
+if a.atomic == true
+    m = mex_gridding3D_adj_atomic_f(single(kspace),(a.dataIndices),single(a.coords),(a.sectorDataCount),(a.sectorProcessingOrder),(a.sectorCenters(:)),single(a.densSorted),single(a.sens),a.params);
+else
+    m = mex_gridding3D_adj_f(single(kspace),(a.dataIndices),single(a.coords),(a.sectorDataCount),(a.sectorProcessingOrder),(a.sectorCenters(:)),single(a.densSorted),single(a.sens),a.params);
+end;
+
+% generate complex output from split vector
+if (a.params.is2d_processing)
+    ress = squeeze(m(1,:,:,:) + 1i*(m(2,:,:,:)));
+else
+    ress = squeeze(m(1,:,:,:,:) + 1i*(m(2,:,:,:,:)));
+end
+
--- matlab/GRIDDING3D/gridding/gridding3D_forw.m
+++ matlab/GRIDDING3D/gridding/gridding3D_forw.m
@@ -1,47 +1,45 @@
 function ress = gridding3D_forw(a,bb)
 % ress = gridding3D_forw(a,bb)
 % Performs forward gridding 
-% from image space to k-space
+% from image to k-space
 %
 % supports multi-channel data
 %
 % a  ... Gridding Operator
 % bb ... image data
+%        W x H x D x (nChn) for 3d 
+%        W x H x (nChn)     for 2d  
 %
 
 %check if imgDims are 2d or 3d 
-%size(bb)
 if (a.params.is2d_processing)
-    n_chnls = size(bb,3);
+    nChn = size(bb,3);
 else
-    n_chnls = size(bb,4);
+    nChn = size(bb,4);
 end
 
+% split and prepare data
 bb = [real(bb(:))'; imag(bb(:))'];
+bb = reshape(bb,[2 a.params.img_dims(1)*a.params.img_dims(2)*max(1,a.params.img_dims(3)) nChn]);
 
-%bb = reshape(bb,[2 prod(a.params.img_dims) n_chnls]);
-bb = reshape(bb,[2 a.params.img_dims(1)*a.params.img_dims(2)*max(1,a.params.img_dims(3)) n_chnls]);
-if (n_chnls > 1)
-    if a.verbose
+if a.verbose
+    if (nChn > 1)
         disp('multiple channel image data passed');
     end
-end    
-if a.verbose
-    disp('call forward gridding mex kernel');
+ disp('call forward gridding mex kernel');
 end
+
 if a.atomic == true
-    %data = mex_gridding3D_forw_atomic_f(single(bb),uint32(a.dataIndices),single(a.coords),uint32(a.sectorDataCount),uint32(a.sectorProcessingOrder),uint32(a.sectorCenters(:)),a.params);
     data = mex_gridding3D_forw_atomic_f(single(bb),(a.dataIndices),single(a.coords),(a.sectorDataCount),(a.sectorProcessingOrder),(a.sectorCenters(:)),single(a.sens),a.params);
 else
-    %data = mex_gridding3D_forw_f(single(bb),uint32(a.dataIndices),single(a.coords),uint32(a.sectorDataCount),uint32(a.sectorProcessingOrder),uint32(a.sectorCenters(:)),a.params);
     data = mex_gridding3D_forw_f(single(bb),(a.dataIndices),single(a.coords),(a.sectorDataCount),(a.sectorProcessingOrder),(a.sectorCenters(:)),single(a.sens),a.params);
 end
-%put data in correct order
-%data_test = zeros(1,length(a.op.data_ind));
+
 if a.verbose
     disp(['returned data dimensions:' num2str(size(data))]);
 end
-if (n_chnls > 1)
+
+if (nChn > 1)
     data = squeeze(data(1,:,:) + 1j*(data(2,:,:)));
     ress(:,:) = data;
 else

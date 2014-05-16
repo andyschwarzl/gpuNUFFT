c54e8e4d60609d8ab1f9ebdd52098f1f69635266
Andi <andy.schwarzl@gmail.com>
2014-05-16 14:17:07 +0200

    Removed Tikreg code (Freiburg).

--- gpuNUFFT/@gpuNUFFT/mtimes.m
+++ gpuNUFFT/@gpuNUFFT/mtimes.m
@@ -1,23 +1,6 @@
 function res = mtimes(a,bb)
-if strcmp(a.method,'sparse')
-    if (a.adjoint)
-        %bb = bb(:);
-        res = Tikreg_gpuNUFFT(a.op,bb,'machine','gpu_float','adjoint');%, ,'single_coil''verbose',1);
-        
-        %TODO check
-        res = res / sqrt(prod(a.imageDim));
-        %END
-    else
-        res = Tikreg_gpuNUFFT(a.op,bb,'machine','gpu_float','verbose','1');%, 'verbose',1);
-        
-        %TODO check
-        res = res / sqrt(prod(a.imageDim));
-        %END
-    end    
-elseif strcmp(a.method,'gpuNUFFT')
-    if (a.adjoint)
-        res = gpuNUFFT_adj(a.op,bb);
-    else
-        res = gpuNUFFT_forw(a.op,bb);
-    end
+if (a.adjoint)
+  res = gpuNUFFT_adj(a.op,bb);
+else
+  res = gpuNUFFT_forw(a.op,bb);
 end

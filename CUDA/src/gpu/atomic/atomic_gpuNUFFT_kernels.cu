fc39797d5447044245d45dcf6eb089245b5404f8
Andi <andy.schwarzl@gmail.com>
2014-05-20 16:39:25 +0200

    Fix allocation of shared memory.

--- CUDA/src/gpu/atomic/atomic_gpuNUFFT_kernels.cu
+++ CUDA/src/gpu/atomic/atomic_gpuNUFFT_kernels.cu
@@ -1380,7 +1380,7 @@ void performForwardConvolution( CufftType*		data_d,
   else
   {
     int thread_size =THREAD_BLOCK_SIZE;
-    long shared_mem_size = thread_size * sizeof(CufftType);//empiric
+    long shared_mem_size = (thread_size + gi_host->sector_dim) * sizeof(CufftType);//empiric
 
     dim3 block_dim(thread_size);
     dim3 grid_dim(getOptimalGridDim(gi_host->sector_count,thread_size));

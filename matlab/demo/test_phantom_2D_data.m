302f6df3009d95b33873cc841d1c8fc95254d4d0
Andi <andy.schwarzl@gmail.com>
2014-06-02 12:30:13 +0200

    Added verbose flag.

--- matlab/demo/test_phantom_2D_data.m
+++ matlab/demo/test_phantom_2D_data.m
@@ -10,6 +10,8 @@ addpath(genpath('../../../fessler/NUFFT'));
 %% Load data
 load sl2d
 
+verbose = false;
+
 %% generate Fourier sampling operator
 osf =2;
 wg = 3;
@@ -18,7 +20,7 @@ imwidth = imgDim(1);
 %%
 disp('init gpu op');
 tic
-FT = gpuNUFFT(k_traj',dens,osf,wg,sw,imgDim,[],false,true,true);
+FT = gpuNUFFT(k_traj',dens,osf,wg,sw,imgDim,[],true,true,true);
 toc
 disp('init cpu op');
 tic
@@ -48,9 +50,11 @@ imgRecon2CPU = FTCPU'*data_reconCPU;
 toc
 
 %% show results
-figure, h1 = subplot(121); imshow(imrotate(abs(imgRecon(:,:)),90),[]), title('Recon');
-h2 = subplot(122); imshow(imrotate(abs(imgReconCPU(:,:)),90),[]), title('CPU Recon');
-linkaxes([h1 h2]);
-figure, h1 = subplot(121); imshow(imrotate(abs(imgRecon2(:,:)),90),[]), title('Recon2');
-h2 = subplot(122); imshow(imrotate(abs(imgRecon2CPU(:,:)),90),[]), title('CPU Recon2');
-linkaxes([h1 h2]);
+if verbose
+  figure, h1 = subplot(121); imshow(imrotate(abs(imgRecon(:,:)),90),[]), title('Recon');
+  h2 = subplot(122); imshow(imrotate(abs(imgReconCPU(:,:)),90),[]), title('CPU Recon');
+  linkaxes([h1 h2]);
+  figure, h1 = subplot(121); imshow(imrotate(abs(imgRecon2(:,:)),90),[]), title('Recon2');
+  h2 = subplot(122); imshow(imrotate(abs(imgRecon2CPU(:,:)),90),[]), title('CPU Recon2');
+  linkaxes([h1 h2]);
+end

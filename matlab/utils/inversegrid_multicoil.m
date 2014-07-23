a6b1b80b75cb6f4b7d731884b5405a9b7e759678
Andi <andy.schwarzl@gmail.com>
2014-07-23 16:21:58 +0200

    Distinguish between 2d and 3d images.

--- matlab/utils/inversegrid_multicoil.m
+++ matlab/utils/inversegrid_multicoil.m
@@ -2,9 +2,13 @@ function data = inversegrid_multicoil(img, FT, nIntl,nRO)
 % 
 % Inverse gpuNUFFT wrapper for multicoil data
 
-[nx,ny,nz,nCh]=size(img);
+if (length(size(img))>3)
+    [nx,ny,nz,nCh]=size(img);
+else
+    [nx,ny,nCh]=size(img);
+end
 
 for ii = 1:nCh
-    data(:,ii) = FT*img(:,:,:,ii);
+    data(:,ii) = FT*img(:,:,ii);
 end
 data=reshape(data,nIntl,nRO,nCh);

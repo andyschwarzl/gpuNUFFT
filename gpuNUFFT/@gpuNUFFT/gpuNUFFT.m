4953e588e863ab1d4b695480f11464af45bf385a
Andi <andy.schwarzl@gmail.com>
2014-07-31 16:25:42 +0200

    Check compute capability of default graphics device and set default value for atomic operation automatically.

--- gpuNUFFT/@gpuNUFFT/gpuNUFFT.m
+++ gpuNUFFT/@gpuNUFFT/gpuNUFFT.m
@@ -21,7 +21,13 @@ function [res] = gpuNUFFT(k,w,osf,wg,sw,imageDim,sens,varargin)
 %  A. Schwarzl, Graz University of Technology
 %  F. Knoll, NYU School of Medicine
 %
-atomic = true;
+t = gpuDevice;
+if eval(t.ComputeCapability) < 2.0
+    atomic = false;
+else
+    atomic = true;
+end
+
 use_textures = true;    
 balance_workload = true;
 

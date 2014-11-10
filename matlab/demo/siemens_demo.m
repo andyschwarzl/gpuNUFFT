d75f3b062588253af017dc82f62f0d7b3a172590
Andreas Schwarzl <andy.schwarzl@gmail.com>
2014-11-10 15:19:00 +0100

    Add CPU NUFFT reference.

--- matlab/demo/siemens_demo.m
+++ matlab/demo/siemens_demo.m
@@ -3,24 +3,26 @@ clear all; close all; clc; clear classes;
 
 addpath(genpath('./utils'));
 addpath(genpath('../../gpuNUFFT'));
+addpath(genpath('../../../fessler/NUFFT'));
 
 %% Data parameters
 N=256;
 
 [x,y] = meshgrid(linspace(-0.5,0.5,N),linspace(-0.5,0.5,N));
 v = linspace(0,1,N);
-v = v'*v;
-figure,imshow(v);
-figure,surf(x,y,v);
+v = 1i*(v'*v);
+figure,imshow(imag(v));
+figure,surf(x,y,imag(v));
 
 k = [col(x)'; col(y)'];
 w = col(ones(size(v)));
 %%
 
 disp('Generate NUFFT Operator without coil sensitivities');
-osf = 2; wg = 3; sw = 10;
+osf = 2; wg = 2; sw = 16;
 imwidth = N;
-FT = gpuNUFFT(k',w,osf,wg,sw,[N,N],[],false,false,false);
+FT = gpuNUFFT(k',w,osf,wg,sw,[N,N],[],true,true,true);
+FTCPU = NUFFT(k(1,:) + 1i*k(2,:),w,1,0,[N,N],2);
 
 %% Forward and adjoint transform
 tic
@@ -34,12 +36,27 @@ disp(['Time adjoint: ', num2str(timeFTH), ' s']);
 
 % Display
 figure;
-subplot(1,2,1); imshow(abs(img_comb(:,:)),[]); title('Regridding');
+subplot(1,2,1); imshow(imag(img_comb(:,:)),[]); title('Regridding');
 subplot(1,2,2); imshow(abs(v(:,:)),[]); title('orig');
+figure, plot(imag(img_comb(6,:)));title('Line 6 - gpuNUFFT');
 
-%%
-figure, plot(abs(img_comb(6,:)))
+%% Forward and adjoint transform NUFFT
+tic
+test = FTCPU*(v);
+timeFT = toc;
+disp(['Time forward: ', num2str(timeFT), ' s']);
+tic
+img_comb_CPU = (FTCPU'*(test));
+timeFTH = toc;
+disp(['Time adjoint: ', num2str(timeFTH), ' s']);
 
+% Display
+figure;
+subplot(1,2,1); imshow(imag(img_comb_CPU(:,:)),[]); title('Regridding NUFFT');
+subplot(1,2,2); imshow(abs(v(:,:)),[]); title('orig');
+figure, plot(imag(img_comb_CPU(6,:)));title('Line 6 - NUFFT');
+
+figure, plot(imag(img_comb_CPU(6,:)./max(col(img_comb_CPU(6,:)))-img_comb(6,:)./max(col(img_comb(6,:))))); title('Line 6 DIFF - NUFFT - gpuNUFFT');
 %% fft referenz
 ref_k = fft2(v);
 ref_img = ifftshift(ifft2(reshape(ifftshift(test),[N N])));

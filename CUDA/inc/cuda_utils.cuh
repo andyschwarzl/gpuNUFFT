0bbfcc3dccf5f7fc204de94a429cbecc4d9dccc6
Andreas Schwarzl <andy.schwarzl@gmail.com>
2014-11-17 22:17:35 +0100

    Add atomic add for double types.

--- CUDA/inc/cuda_utils.cuh
+++ CUDA/inc/cuda_utils.cuh
@@ -76,4 +76,17 @@ __inline__ __device__ DType computeTextureLookup(DType x, DType y, DType z)
 	#define THREAD_BLOCK_SIZE 256
 #endif
 
+// From NVIDIA devtalk
+__inline__ __device__ double atomicAdd(double* address, double val)
+{
+  unsigned long long int* address_as_ull = (unsigned long long int*) address;
+  unsigned long long int old = *address_as_ull, assumed;
+  do {
+    assumed = old;
+    old = atomicCAS(address_as_ull, assumed, __double_as_longlong(val+__longlong_as_double(assumed)));
+  }  
+  while (assumed != old);
+  return __longlong_as_double(old);
+}
+
 #endif

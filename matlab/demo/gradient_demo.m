4ae80d2f402af361f5b2a70238b8435639e693b6
Andi <andy.schwarzl@gmail.com>
2014-11-22 17:08:12 +0100

    Minor: changes.

--- matlab/demo/gradient_demo.m
+++ matlab/demo/gradient_demo.m
@@ -13,7 +13,7 @@ N=256;
 k = [col(x)'; col(y)'];
 
 %non-smooth version
-[x,y] = meshgrid(linspace(-0.5,0.5,N),linspace(-0.5,0.5,N));
+[x_ns,y_ns] = meshgrid(linspace(-0.5,0.5,N),linspace(-0.5,0.5,N));
 k_nonsmooth = [col(x)'; col(y)'];
 
 v = linspace(0,1,N);
@@ -26,16 +26,16 @@ w = col(ones(size(v)));
 disp('Generate NUFFT Operator');
 osf = 2;
 kw = 3; %1 also possible and ideal for uniform cartesian 
-sw = 8;
+sw = 5;
 imwidth = N;
 
-FT = gpuNUFFT(k',w,osf,kw,sw,[N,N],[]);
+FT = gpuNUFFT(k',w,osf,kw,sw,[N,N],[],false,false,false);
 FTCPU = NUFFT(k(1,:) + 1i*k(2,:),w,1,0,[N,N],2);
 
 run_demo(v,FT,FTCPU,x,y,N,'Smooth ');
 %% non smooth recon
 kw = 3; %nonsmooth
-FT = gpuNUFFT(k_nonsmooth',w,osf,kw,sw,[N,N],[]);
+FT = gpuNUFFT(k_nonsmooth',w,osf,kw,sw,[N,N],[],false,false,false);
 FTCPU = NUFFT(k_nonsmooth(1,:) + 1i*k_nonsmooth(2,:),w,1,0,[N,N],2);
 
-run_demo(v,FT,FTCPU,x,y,N,'Non-smooth ');
+run_demo(v,FT,FTCPU,x_ns,y_ns,N,'Non-smooth ');

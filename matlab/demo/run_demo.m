8b88db0276a5487202e57f8d326e078b15fcd846
Andreas Schwarzl <andy.schwarzl@gmail.com>
2014-11-28 13:16:59 +0100

    Exclude shift tests.

--- matlab/demo/run_demo.m
+++ matlab/demo/run_demo.m
@@ -11,11 +11,11 @@ disp(['Time adjoint: ', num2str(timeFTH), ' s']);
 
 %% Forward and adjoint transform NUFFT
 tic
-test = FTCPU*(v);
+test2 = FTCPU*(v);
 timeFT = toc;
 disp(['Time forward: ', num2str(timeFT), ' s']);
 tic
-img_comb_CPU = (FTCPU'*(test));
+img_comb_CPU = (FTCPU'*(test2));
 timeFTH = toc;
 disp(['Time adjoint: ', num2str(timeFTH), ' s']);
 %% fft reference
@@ -23,6 +23,11 @@ ref_k = fft2(v);
 ref_img = (ifft2(reshape((ref_k),[N N])));
 
 %% Compare results
+figure;
+h1=subplot(1,2,1); imshow(log(abs(reshape(test,[N N]))),[]); title([demoTitlePrefix 'kspace gpuNUFFT']);colorbar;
+h2=subplot(1,2,2); imshow(log(abs(reshape(test2,[N N]))),[]); title([demoTitlePrefix 'kspace NUFFT']);colorbar;
+linkaxes([h1 h2]);
+
 figure;
 h1=subplot(1,3,1); imshow(imag(img_comb),[]); title([demoTitlePrefix 'Regridding gpuNUFFT']);
 h2=subplot(1,3,2); imshow(imag(img_comb_CPU),[]); title([demoTitlePrefix 'Regridding NUFFT']);

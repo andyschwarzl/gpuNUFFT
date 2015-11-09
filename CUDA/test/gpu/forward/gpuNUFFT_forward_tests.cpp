3476a9b9e31c4d39c15e8344c9889ea6d618e939
Andreas Schwarzl <andy.schwarzl@gmail.com>
2015-11-09 20:21:39 +0100

    Add 2d forward test with sens data.

--- CUDA/test/gpu/forward/gpuNUFFT_forward_tests.cpp
+++ CUDA/test/gpu/forward/gpuNUFFT_forward_tests.cpp
@@ -223,10 +223,9 @@ TEST(TestGPUGpuNUFFTForwardConv, 2D_32_32_4)
     bool useGpu = cnt & 2;
     bool loadBalancing = cnt & 4;
 
-    if (DEBUG)
-      std::cout << "Use Textures: " << useTextures << std::endl
-                << "Use GPU: " << useGpu << std::endl
-                << "Use LoadBalancing:" << loadBalancing << std::endl;
+    std::cout << "Use Textures: " << useTextures << std::endl
+              << "Use GPU: " << useGpu << std::endl
+              << "Use LoadBalancing:" << loadBalancing << std::endl;
 
     gpuNUFFT::GpuNUFFTOperatorFactory factory(useTextures, useGpu,
                                               loadBalancing);
@@ -247,16 +246,11 @@ TEST(TestGPUGpuNUFFTForwardConv, 2D_32_32_4)
 
     for (int chn = 0; chn < n_coils; chn++)
     {
-      // EXPECT_NEAR(0.01358f, dataArray.data[0 + chn * data_count].x, epsilon);
-      if (DEBUG)
-      {
-        printf("x: %.5f %.5f \n", dataArray.data[0].x,
-               dataArray.data[0 + chn * data_count].x);
-        printf("y: %.5f %.5f \n", dataArray.data[0].y,
-               dataArray.data[0 + chn * data_count].y);
-      }
-      EXPECT_EQ(dataArray.data[0].x, dataArray.data[0 + chn * data_count].x);
-      EXPECT_EQ(dataArray.data[0].y, dataArray.data[0 + chn * data_count].y);
+      EXPECT_NEAR(3.2127f, dataArray.data[0 + chn * data_count].x, epsilon);
+      EXPECT_NEAR(dataArray.data[0].x, dataArray.data[0 + chn * data_count].x,
+                  1E-5);
+      EXPECT_NEAR(dataArray.data[0].y, dataArray.data[0 + chn * data_count].y,
+                  1E-5);
     }
 
     if (DEBUG)
@@ -265,6 +259,129 @@ TEST(TestGPUGpuNUFFTForwardConv, 2D_32_32_4)
   }
 
   free(coords);
+  free(im_dataArray.data);
+}
+
+TEST(TestGPUGpuNUFFTForwardConv, 2D_32_32_8_sens)
+{
+  int kernel_width = 5;
+  float osr = 1.25f;
+  int im_width = 32;
+
+  // Data
+  int data_count = 3;
+  int n_coils = 8;
+
+  // Coords
+  // Scaled between -0.5 and 0.5
+  // in duplets (x,y)
+  DType *coords = (DType *)calloc(2 * data_count, sizeof(DType));  // 2* x,y,z
+  coords[0] = -0.31719f;
+  coords[1] = -0.38650f;
+
+  coords[2] = 0;
+  coords[3] = 0;
+
+  coords[4] = 0.25;
+  coords[5] = 0.25;
+
+  gpuNUFFT::Array<DType2> im_dataArray;
+  im_dataArray.data = NULL;
+  im_dataArray.dim.width = im_width;
+  im_dataArray.dim.height = im_width;
+  im_dataArray.dim.channels = 1;
+
+  im_dataArray.data = (DType2 *)calloc(im_dataArray.count(), sizeof(DType2));
+
+  for (unsigned x = 0; x < im_dataArray.count(); x++)
+  {
+    im_dataArray.data[x].x = 1.0f;
+    im_dataArray.data[x].y = 1.0f;
+  }
+
+  gpuNUFFT::Array<DType2> sensArray;
+  sensArray.data = NULL;
+  sensArray.dim.width = im_width;
+  sensArray.dim.height = im_width;
+  sensArray.dim.channels = n_coils;
+
+  sensArray.data = (DType2 *)calloc(sensArray.count(), sizeof(DType2));
+
+  for (int coil = 0; coil < n_coils; coil++)
+  {
+    for (unsigned x = 0; x < im_dataArray.count(); x++)
+    {
+      sensArray.data[coil * im_dataArray.count() + x].x = (DType)coil + 1;
+      sensArray.data[coil * im_dataArray.count() + x].y = (DType)coil + 1;
+    }
+  }
+
+  gpuNUFFT::Array<DType> densArray;
+  densArray.data = (DType *)calloc(data_count, sizeof(DType));
+  densArray.dim.length = data_count;
+  for (int cnt = 0; cnt < data_count; cnt++)
+    densArray.data[cnt] = 1.0;
+
+  // sectors of data, count and start indices
+  int sector_width = 8;
+
+  gpuNUFFT::Array<DType> kSpaceData;
+  kSpaceData.data = coords;
+  kSpaceData.dim.length = data_count;
+
+  gpuNUFFT::Array<CufftType> dataArray;
+
+  // imgDims -> no Channel size here
+  gpuNUFFT::Dimensions imgDims;
+  imgDims.width = im_dataArray.dim.width;
+  imgDims.height = im_dataArray.dim.height;
+
+  for (int cnt = 0; cnt < 8; cnt++)
+  {
+    bool useTextures = cnt & 1;
+    bool useGpu = cnt & 2;
+    bool loadBalancing = cnt & 4;
+
+    std::cout << "Use Textures: " << useTextures << std::endl
+              << "Use GPU: " << useGpu << std::endl
+              << "Use LoadBalancing:" << loadBalancing << std::endl;
+
+    gpuNUFFT::GpuNUFFTOperatorFactory factory(useTextures, useGpu,
+                                              loadBalancing);
+    gpuNUFFT::GpuNUFFTOperator *gpuNUFFTOp = factory.createGpuNUFFTOperator(
+        kSpaceData, densArray, sensArray, kernel_width, sector_width, osr,
+        imgDims);
+
+    dataArray = gpuNUFFTOp->performForwardGpuNUFFT(im_dataArray);
+
+    if (DEBUG)
+    {
+      std::cout << "result count : " << dataArray.count() << std::endl;
+
+      for (unsigned j = 0; j < dataArray.count(); j++)
+      {
+        printf("x: %.6f %.6f \n", dataArray.data[j].x, dataArray.data[j].y);
+      }
+    }
+
+    EXPECT_EQ(dataArray.dim.channels, n_coils);
+
+    for (int chn = 1; chn < n_coils; chn++)
+    {
+      EXPECT_NEAR((chn + 1) * dataArray.data[0].x,
+                  dataArray.data[0 + chn * data_count].x, 1E-3);
+      EXPECT_NEAR((chn + 1) * dataArray.data[0].y,
+                  dataArray.data[0 + chn * data_count].y, 1E-3);
+    }
+
+    if (DEBUG)
+      printf("\n");
+    delete gpuNUFFTOp;
+  }
+
+  free(coords);
+  free(densArray.data);
+  free(sensArray.data);
   free(im_dataArray.data);
 }
 

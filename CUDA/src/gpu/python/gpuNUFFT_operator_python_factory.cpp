f49b11ea4b85d0b9ac8a2f28806f5ae7a8fd5717
chaithyagr <chaithyagr@gitlab.com>
2020-03-18 12:52:04 +0100

    WIP, second channel not working

--- CUDA/src/gpu/python/gpuNUFFT_operator_python_factory.cpp
+++ CUDA/src/gpu/python/gpuNUFFT_operator_python_factory.cpp
@@ -43,10 +43,12 @@ class GpuNUFFTPythonOperator
     gpuNUFFT::GpuNUFFTOperatorFactory factory;
     gpuNUFFT::GpuNUFFTOperator *gpuNUFFTOp;
     int trajectory_length, n_coils, dimension;
+    bool has_sense_data;
     gpuNUFFT::Dimensions imgDims;
     public:
-    GpuNUFFTPythonOperator(py::array_t<DType> kspace_loc, py::array_t<int> image_size, py::array_t<std::complex<DType>> sense_maps,
-    py::array_t<float> density_comp, int kernel_width=3, int sector_width=8, int osr=2, bool balance_workload=1)
+    GpuNUFFTPythonOperator(py::array_t<DType> kspace_loc, py::array_t<int> image_size, int num_coils,
+    py::array_t<std::complex<DType>> sense_maps,  py::array_t<float> density_comp, int kernel_width=3,
+    int sector_width=8, int osr=2, bool balance_workload=1)
     {
         // k-space coordinates
         py::buffer_info sample_loc = kspace_loc.request();
@@ -69,23 +71,26 @@ class GpuNUFFTPythonOperator
         else
             imgDims.depth = 0;
 
+        n_coils = num_coils;
+
         // sensitivity maps
         gpuNUFFT::Array<DType2> sensArray;
-        DType2 *sensData = NULL;
         py::buffer_info sense_maps_buffer = sense_maps.request();
-
-        if (sense_maps_buffer.size==0)
-            n_coils = 1;
+        printf("sense buffer size : %d\n",  sense_maps_buffer.size);
+        if (sense_maps_buffer.shape.size()==0)
+        {
+            has_sense_data = false;
+            printf("It is NULL\n");
+            sensArray.data = NULL;
+        }
         else
         {
-            n_coils = sense_maps_buffer.shape[0];
-            sensData = (DType2 *) sense_maps_buffer.ptr;
+            printf("With Sens Array\n");
+            sensArray = readNumpyArray(sense_maps);
+            sensArray.dim = imgDims;
+            sensArray.dim.channels = n_coils;
+            has_sense_data = true;
         }
-
-        sensArray.data = sensData;
-        sensArray.dim = imgDims;
-        sensArray.dim.channels = n_coils;
-
         factory.setBalanceWorkload(balance_workload);
         gpuNUFFTOp = factory.createGpuNUFFTOperator(
             kSpaceTraj, density_compArray, sensArray, kernel_width, sector_width,
@@ -116,14 +121,19 @@ class GpuNUFFTPythonOperator
         int depth = imgDims.depth;
         if(dimension==2)
             depth = 1;
-        py::array_t<std::complex<DType>> out_result({depth, (int)imgDims.height, (int)imgDims.width});
+        py::array_t<std::complex<DType>> out_result;
+        if(has_sense_data == false)
+            out_result.resize({n_coils, depth, (int)imgDims.height, (int)imgDims.width});
+        else
+            out_result.resize({depth, (int)imgDims.height, (int)imgDims.width});
         py::buffer_info out = out_result.request();
         std::complex<DType> *t_data = (std::complex<DType> *) out.ptr;
         DType2 *new_data = reinterpret_cast<DType2(&)[0]>(*t_data);
         gpuNUFFT::Array<DType2> imdataArray;
         imdataArray.data = new_data;
         imdataArray.dim = imgDims;
-
+        if(has_sense_data == false)
+            imdataArray.dim.channels = n_coils;
         gpuNUFFT::Array<CufftType> dataArray = readNumpyArray(kspace_data);
         dataArray.dim.length = trajectory_length;
         gpuNUFFTOp->performGpuNUFFTAdj(dataArray, imdataArray);
@@ -137,7 +147,7 @@ class GpuNUFFTPythonOperator
 };
 PYBIND11_MODULE(gpuNUFFT, m) {
     py::class_<GpuNUFFTPythonOperator>(m, "NUFFTOp")
-        .def(py::init<py::array_t<float>, py::array_t<int>, py::array_t<std::complex<float>>, py::array_t<float>, int, int, int, bool>())
+        .def(py::init<py::array_t<DType>, py::array_t<int>, int, py::array_t<std::complex<DType>>, py::array_t<float>, int, int, int, bool>())
         .def("op", &GpuNUFFTPythonOperator::op)
         .def("adj_op",  &GpuNUFFTPythonOperator::adj_op);
 }

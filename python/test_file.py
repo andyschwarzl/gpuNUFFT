07d0cab1431c2064afe59a81ef5f019c8390fa46
chaithyagr <chaithyagr@gitlab.com>
2021-11-18 14:46:19 +0100

    New test

--- python/test_file.py
+++ python/test_file.py
@@ -1,13 +1,11 @@
 import numpy as np
 from mri.operators import NonCartesianFFT
-from mri.operators.fourier.utils import estimate_density_compensation
-
 
 traj = np.load('/volatile/temp_traj.npy')
-for i in range(5):
+
+fourier = NonCartesianFFT(traj, (384, 384, 208), 'gpuNUFFT', n_coils=20, smaps=np.ones((20, 384, 384, 208)), osf=2)
+
+for i in range(10):
     print(i)
-    fourier = NonCartesianFFT(traj, (384, 384, 208), 'gpuNUFFT', n_coils=10, smaps=np.ones((10, 384, 384, 208)))
-    fourier.impl.operator.set_smaps(np.ones((10, 384, 384, 208))+1)
     K = fourier.op(np.zeros((384, 384, 208)))
-    im = fourier.adj_op(K)
-    del fourier
+    I = fourier.adj_op(K)
